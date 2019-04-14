fb0d5033505f4dde26ab1bf719b6b03101154481
fisherek15 <fisherek15@gmail.com>
2019-04-14 20:49:46 +0200

    Finished the first version! Commented unnecessary lines of code.

--- main/views.py
+++ main/views.py
@@ -1,6 +1,6 @@
-from io import BytesIO
-from reportlab.pdfgen import canvas
-from django.http import HttpResponse
+# from io import BytesIO
+# from reportlab.pdfgen import canvas
+# from django.http import HttpResponse
 from datetime import datetime
 from django.shortcuts import render, redirect, get_object_or_404
 from django.http import HttpResponse
